14d824bfb158e09adf47c432e4f1b077bb799d82
biel-aguma-cosa <bielgferraz@gmail.com>
2026-03-02 18:42:35 -0300

    settings updated + temperaturas_2.py

---
+++ outros/temperaturas_2.py
@@ -0,0 +1,15 @@
+import streamlit as st # type: ignore
+
+st.title('Hello World')
+st.write('''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Etiam eget ligula eu lectus lobortis condimentum.
+Aliquam nonummy auctor massa.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+Nulla at risus.
+Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus.
+Nam mattis, felis ut adipiscing''')
+
+
+# 21981762949
+
