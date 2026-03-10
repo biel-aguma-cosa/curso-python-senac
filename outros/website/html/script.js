31d1cd7602697accd5cc4860d514f2b69538f5c5
biel-aguma-cosa <bielgferraz@gmail.com>
2026-03-10 11:59:21 -0300

    login atualizado

--- outros/website/html/script.js
+++ outros/website/html/script.js
@@ -1,32 +1,3 @@
-const sock = {
-    'text_field' : document.getElementById('sock_field'),
-    'entry_field': document.getElementById('sock_text')
-}
-const targets = {
-        "slider" : {
-            "K" : document.getElementById('K_slider'),
-            "F" : document.getElementById('F_slider'),
-            "C" : document.getElementById('C_slider')
-        },
-        "num" : {
-            'K': document.getElementById('K_num'),
-            'F': document.getElementById('F_num'),
-            'C': document.getElementById('C_num')
-        },
-        "index" : {
-            'C': 0,
-            'K': 1,
-            'F': 2
-        }
-    }
-const temp_vals = {
-        "K" : 0,
-        "F" : 0,
-        "C" : 0
-    }
-const dummy = ['C','K','F'];
-        
-
 function open_tab(event,tab) {
     let i, tab_content, tab_buttons
     tab_content = document.getElementsByClassName('content');
@@ -42,30 +13,32 @@ function open_tab(event,tab) {
     button.className += ' active'
 }
 function temperature(type,index) {
+    const dummy = ['K','F','C']
     switch (index) {
         case 0:
-            temp_vals.C = Number(targets[type].C.value)
-            temp_vals.K = Math.round(temp_vals.C + 273.15)
-            temp_vals.F = Math.round(temp_vals.C * 9/5 + 32)
+            globalThis.temp_vals.C = Number(globalThis.targets[type].C.value)
+            globalThis.temp_vals.K = globalThis.temp_vals.C + 273.15  
+            globalThis.temp_vals.F = globalThis.temp_vals.C * 9/5 + 32
             break
         case 1:
-            temp_vals.K = Number(targets[type].K.value)
-            temp_vals.C = Math.round(temp_vals.K - 273.15)
-            temp_vals.F = Math.round((temp_vals.C * 1.8) + 32)
+            globalThis.temp_vals.K = Number(globalThis.targets[type].K.value)
+            globalThis.temp_vals.C = globalThis.temp_vals.K - 273.15    
+            globalThis.temp_vals.F = (globalThis.temp_vals.C * 1.8) + 32
             break
         case 2:
-            temp_vals.F = Number(targets[type].F.value)
-            temp_vals.C = Math.round((temp_vals.F - 32)*5/9)
-            temp_vals.K = Math.round(temp_vals.C + 273.15)
+            globalThis.temp_vals.F = Number(globalThis.targets[type].F.value)
+            globalThis.temp_vals.C = (globalThis.temp_vals.F - 32)*5/9
+            globalThis.temp_vals.K = globalThis.temp_vals.C + 273.15  
             break
     }
     for (let i of dummy) {
-        targets.num[i].value = temp_vals[i]
-        targets.slider[i].value = temp_vals[i]
+        globalThis.targets.num[i].value    = parseFloat(globalThis.temp_vals[i]).toFixed(2)
+        globalThis.targets.slider[i].value = parseFloat(globalThis.temp_vals[i]).toFixed(2)
     }
 }
 function message(data,type,sender) {
     let _div = document.createElement('div')
+    let div = document.createElement('div')
     let text = document.createElement('p')
     let user = document.createElement('p')
     _div.style.height = '15px'
@@ -76,30 +49,38 @@ function message(data,type,sender) {
     div.appendChild(text)
     _div.appendChild(user)
     
-    text_field.appendChild(div)
-    text_field.appendChild(_div)
+    globalThis.sock.text_field.appendChild(div)
+    globalThis.sock.text_field.appendChild(_div)
 }
 function speak() {
-    let entry = sock.entry.value
-    if (user_data.username && user_data.password) {
+    let entry = globalThis.sock.entry_field.value
+    if (globalThis.user_data.username && globalThis.user_data.password) {
         socket.send(JSON.stringify(
             {
-                type : 'message',
-                message : entry,
-                user_data : user_data
+                'type' : 'message',
+                'message' : entry,
+                'user_data' : globalThis.user_data
             }
         ))
     }
 }
 
 function login() {
-    const socket = new WebSocket("ws://localhost:55555")
+    globalThis.socket = new WebSocket("ws://localhost:55555")
+    socket = globalThis.socket
 
+    console.log(globalThis.user_data)
+    console.log(JSON.stringify(
+            {
+                'type' : 'login',
+                'user' : globalThis.user_data
+            }
+        ))
     socket.onopen = function () {
         socket.send(JSON.stringify(
             {
                 'type' : 'login',
-                'user' : user_data
+                'user' : globalThis.user_data
             }
         ))
     }
@@ -112,7 +93,7 @@ function login() {
                 }
                 break
             case 'reload':
-                for (let child of sock.text_field.childNodes) {
+                for (let child of globalThis.sock.text_field.childNodes) {
                     child.remove()
                 }
                 for (let text of data.messages) {
@@ -122,15 +103,15 @@ function login() {
                 }
                 break
             case 'accepted':
-                sock.entry_field.value = ''
+                globalThis.sock.entry_field.value = ''
                 message(data.message,'',sender)
                 break
             case 'login':
-                if (data.username == username) {
+                if (data.username == globalThis.user_data.username) {
                     message(
-                        username+' entrou',
+                        globalThis.user_data.username+' entrou',
                         '',
-                        username)
+                        globalThis.user_data.username)
                 }else{
                     message(
                         data.username+' entrou',
@@ -145,7 +126,7 @@ function login() {
 function login_submission_handler(event) {
     event.preventDefault()
     const data = new FormData(event.target)
-    user = {
+    globalThis.user_data = {
         'username' : data.get('username'),
         'password': data.get('password')
     }
@@ -154,18 +135,31 @@ function login_submission_handler(event) {
 }
 
 function main() {
-    const user_data = {
-        'username' : null,
-        'password' : null
-    }
+    globalThis.targets = {
+        "slider" : {
+            "K" : document.getElementById('K_slider'),
+            "F" : document.getElementById('F_slider'),
+            "C" : document.getElementById('C_slider')},
+        "num" : {
+            'K': document.getElementById('K_num'),
+            'F': document.getElementById('F_num'),
+            'C': document.getElementById('C_num')},
+        "index" : {
+            'C': 0,
+            'K': 1,
+            'F': 2}}
+    globalThis.sock = {'text_field' : document.getElementById('sock_field'),    'entry_field': document.getElementById('sock_text')}
 
-    const socket_tab = document.getElementById('socket_tab')
-    const form = document.getElementById('login_form');
+    globalThis.temp_vals = {"K" : 0,        "F" : 0,        "C" : 0    }
+    globalThis.dummy = ['C','K','F'];
+    globalThis.user_data = {'username' : null,'password' : null}
+
+    globalThis.socket_tab = document.getElementById('socket_tab')
+    globalThis.form = document.getElementById('login_form');
 
     document.getElementById('default').click()
 
     form.addEventListener('submit',login_submission_handler)
-
 
     document.addEventListener('keydown', function (event) {
         if (event.key == 'Enter') {
--- outros/website/python/nada.py
+++ outros/website/python/nada.py
@@ -5,6 +5,7 @@ import websockets
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
+
 def data_read():
     global dir
     with open(os.path.join(dir,'socket_data.json'),'r') as file:
@@ -14,13 +15,16 @@ def data_read():
 def data_write(raw_data,target):
     global dir
     data = data_read()
-    data[target][raw_data['index']] = raw_data['data']
+    if target == 'users':
+        data['users'][raw_data['index']] = raw_data['data']
+    elif target == 'messages':
+        data['messages'].append(raw_data['data'])
+    
     with open(os.path.join(dir,'socket_data.json'),'w') as file:
         dump_data = json.dumps(data,indent=3)
         file.write(dump_data)
         file.close()
-
-print(data_read())
+        
 
 async def conn_handler(socket):
     print('[client connected]')
@@ -33,22 +37,26 @@ async def conn_handler(socket):
                         if data['user']['password'] == data_read()['users'][data['user']['username']]['password']:
                             await socket.send(json.dumps({
                                 'type':'login',
-                                'username':data['user']['username']
+                                'username':data['user']['username'],
+                                'result': 'login'
                             }))
                     else:
-                        data['user']['username'] = data['user']
                         data_write({
                             'data' : data['user'],
                             'index' : data['user']['username'],
                         },'users')
                         await socket.send(json.dumps({
                                 'type':'login',
-                                'username':data['user']['username']
+                                'username':data['user']['username'],
+                                'result': 'register'
                             }))
                 case 'message':
                     pass
                 case 'reload':
-                    pass
+                    await socket.send(json.dumps({
+                                'type':'login',
+                                'username':data['user']['username']
+                            }))
                 case 'update':
                     pass
 
